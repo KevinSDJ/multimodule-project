cbb487dda9a3c1bb45a58db7eba89faa961ba3c1
kevin De jesus <sebadjkevin@gmail.com>
2023-05-08 22:06:15 -0300

    .

--- traditional-rest/src/main/java/com/kevinsdj/traditionalrest/rest/SayHello.java
+++ traditional-rest/src/main/java/com/kevinsdj/traditionalrest/rest/SayHello.java
@@ -1,5 +1,26 @@
+package com.kevinsdj.traditionalrest.rest;
+
+import java.util.concurrent.CompletableFuture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.kevinsdj.traditionalrest.service.SayHelloService;
 
 
+@RestController
 public class SayHello {
     
+    @Autowired
+    private SayHelloService service;
+
+    
+    @GetMapping("/say-hello-async")
+    public CompletableFuture<String> sayHelloAsync(){
+
+        return service.sayHelloAsync();
+    }
+    @GetMapping("/say-hello")
+    public String sayHello(){
+        return service.sayHello();
+    }
 }
--- traditional-rest/src/main/java/com/kevinsdj/traditionalrest/service/SayHelloService.java
+++ traditional-rest/src/main/java/com/kevinsdj/traditionalrest/service/SayHelloService.java
@@ -1,8 +1,24 @@
-import com.kevinsdj.traditionalrest.service;
+package com.kevinsdj.traditionalrest.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class SayHelloService {
 
-
+    @Async
+    public CompletableFuture<String> sayHelloAsync(){
+        
+        return CompletableFuture.completedFuture("async response");
+    }
+    
+    
+    public String sayHello(){
+        
+        return "normal response";
+    }
     
 }
